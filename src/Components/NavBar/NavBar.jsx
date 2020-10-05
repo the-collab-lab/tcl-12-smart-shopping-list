5216e4f86518f745a8ae12a3ef9b2bd7aab8c3e8
Terry Chiem <terrychiem@gmail.com>
2020-10-05 09:17:37 -0700

    Add NavBar component and active link styling

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './NavBar.css';
+
+export default function NavBar() {
+  return (
+    <ul className="NavBar">
+      <li>
+        <NavLink exact to="/" activeClassName="nav-selected">
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink exact to="/list" activeClassName="nav-selected">
+          List
+        </NavLink>
+      </li>
+      <li>
+        <NavLink exact to="/add-item" activeClassName="nav-selected">
+          Add an Item
+        </NavLink>
+      </li>
+    </ul>
+  );
+}
--- src/Components/Routes.jsx
+++ src/Components/Routes.jsx
@@ -1,36 +1,26 @@
 import React from 'react';
-import { Route, BrowserRouter as Router, Link, Switch } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import Home from './Home';
 import List from './List';
 import AddItem from './AddItem';
+import NavBar from './NavBar/NavBar';
 
 export default function Routes() {
   return (
     <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/list">List</Link>
-          </li>
-          <li>
-            <Link to="/add-item">Add an Item</Link>
-          </li>
-        </ul>
-        <Switch>
-          <Route path="/list">
-            <List />
-          </Route>
-          <Route path="/add-item">
-            <AddItem />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
+      <NavBar />
+
+      <Switch>
+        <Route path="/list">
+          <List />
+        </Route>
+        <Route path="/add-item">
+          <AddItem />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
     </Router>
   );
 }
