0776518b56e8d0f22f74e1ca381eeb77b771a9b6
EDEN <EDBEK@YAHOO.COM>
2020-10-06 17:32:59 -0500

    Updated count based on Firestore database
    Co-authored-by: Alexandra Lim <alexandra.lim@gmail.com>
    Co-authored-by: Eden Bekele <edbek@yahoo.com>

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,18 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { db } from '../lib/firebase';
 
 const Counter = () => {
-  const [count, setCount] = useState(0);
-  const increaseCount = () => {
-    setCount(count + 1);
+  const [count, setCount] = useState();
+  const newCount = db.collection('count').doc('FQyeXUHAfge9AXp6qXrK');
+
+  useEffect(() => {
     const newCount = db.collection('count').doc('FQyeXUHAfge9AXp6qXrK');
+    newCount.get().then(function (doc) {
+      setCount(doc.data().number);
+    });
+  }, []);
+
+  const increaseCount = (newCount) => {
+    setCount(count + 1);
     newCount.update({ number: count + 1 });
   };
 
   return (
     <div>
       <p>The count is {count}!</p>
-      <button onClick={increaseCount}>Click</button>
+      <button onClick={() => increaseCount(newCount)}>Click</button>
     </div>
   );
 };
