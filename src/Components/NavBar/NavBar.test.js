5b642532427bf2b2ea85e1e7bb9acbdd2f1c0bab
Terry Chiem <terrychiem@gmail.com>
2020-10-07 15:19:49 -0700

    Fix test for NavBar and update dependencies for testing library. Co-authored: Gabrielle Jacobs <GabbyJ424@gmail.com>

--- src/Components/NavBar/NavBar.test.js
+++ src/Components/NavBar/NavBar.test.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 
 import NavBar from './NavBar';
 
 it('renders without crashing', () => {
-  render(<NavBar />);
+  render(
+    <MemoryRouter>
+      <NavBar />
+    </MemoryRouter>,
+  );
 });
