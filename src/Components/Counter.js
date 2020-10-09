90a50076f8eaaec932efd6520843fee1b1307e49
EDEN <EDBEK@YAHOO.COM>
2020-10-08 19:59:02 -0500

    Added error catching to get request and update request
    Co-authored-by: Eden Bekele <edbek@yahoo.com>
    Co-authored-by: Alexandra Lim <alexandra.lim@gmail.com>

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -6,14 +6,25 @@ const Counter = () => {
   const newCount = db.collection('count').doc('FQyeXUHAfge9AXp6qXrK');
 
   useEffect(() => {
-    newCount.get().then(function (doc) {
-      setCount(doc.data().number);
-    });
+    newCount
+      .get()
+      .then(function (doc) {
+        setCount(doc.data().number);
+      })
+      .catch((error) => {
+        console.log('Error getting document:', error);
+      });
   }, [newCount]);
 
   const increaseCount = (newCount) => {
-    setCount(count + 1);
-    newCount.update({ number: count + 1 });
+    newCount
+      .update({ number: count + 1 })
+      .then(() => {
+        setCount(count + 1);
+      })
+      .catch((error) => {
+        console.log('Error updating document:', error);
+      });
   };
 
   return (
