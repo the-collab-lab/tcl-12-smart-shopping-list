492f9eb0eabbb5f898604aceda1cf8f5951365d2
EDEN <EDBEK@YAHOO.COM>
2020-10-12 12:16:24 -0500

    Added form for user to add an item to a list
    Co-authored-by: Eden Bekele <edbek@yahoo.com>
    Co-authored-by: Terry Chiem <terrychiem@gmail.com>

--- src/Components/AddItem.jsx
+++ src/Components/AddItem.jsx
@@ -1,5 +1,41 @@
 import React from 'react';
 
 export default function AddItem() {
-  return <div>Hi from Add an Item View!</div>;
+  return (
+    <div>
+      <form>
+        <div>
+          <label htmlFor="item-name">Item Name: </label>
+          <br />
+          <input type="text" name="item-name" />
+        </div>
+        <div>
+          <label htmlFor="item-frequency">
+            How soon are you likely to buy again?
+          </label>
+          <br />
+          <input
+            type="radio"
+            id="soon"
+            name="item-frequency"
+            value="7"
+            defaultChecked
+          />
+          <label htmlFor="soon">Soon</label>
+          <br />
+          <input
+            type="radio"
+            id="kind-of-soon"
+            name="item-frequency"
+            value="14"
+          />
+          <label htmlFor="kind-of-soon">Kind of Soon</label>
+          <br />
+          <input type="radio" id="not-soon" name="item-frequency" value="30" />
+          <label htmlFor="not-soon">Not Soon</label>
+        </div>
+      </form>
+      <input type="submit" value="submit" />
+    </div>
+  );
 }
