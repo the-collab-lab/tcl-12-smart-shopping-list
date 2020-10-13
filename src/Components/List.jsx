7a5f558ed119fbfedacdb57a8b5b08fcc9e502fa
Terry Chiem <terrychiem@gmail.com>
2020-10-13 13:47:01 -0700

    Implement loading of firestore list and displaying of items in an unordered list.
    Co-authored-by: Eden Bekele <edbek@yahoo.com>
    Co-authored-by: Terry Chiem <terrychiem@gmail.com>

--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { db } from '../lib/firebase';
 
 // TODO: Read list from firestore and display list items
 
 export default function List() {
-  return <div>Hi from List View!</div>;
+  const [token, setToken] = useState('test-token');
+  const [itemList, setItemList] = useState([]);
+
+  useEffect(() => {
+    // Retrieve item list from Firestore
+    db.collection('lists')
+      .doc(token)
+      .get()
+      .then((doc) => {
+        setItemList(doc.data().items);
+      });
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {itemList.map((item) => {
+          return <li key={item.name}>{item.name}</li>;
+        })}
+      </ul>
+    </div>
+  );
 }
