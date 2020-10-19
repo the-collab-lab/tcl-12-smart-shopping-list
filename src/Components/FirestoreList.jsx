78f5b784c71cc3fe5fd560c37a5daaea27307911
Alexandra Lim <alexandra.lim@gmail.com>
2020-10-19 18:55:13 -0400

    Refactored data in FirestoreList to accept new database structure
    Co-authored-by: Terry Chiem <terrychiem@gmail.com>
    Co-authored-by: Alexandra Lim <alexandra.lim@gmail.com>

--- src/Components/FirestoreList.jsx
+++ src/Components/FirestoreList.jsx
@@ -11,10 +11,16 @@ export default function FirestoreList({ token }) {
     <FirestoreDocument
       path={`lists/${token}`}
       render={({ isLoading, data }) => {
-        // TODO
-        // retrieve items with Object.entries/keys
-        // convert data into an array of objects
-        return isLoading ? <div>Loading</div> : <List items={data.items} />;
+        let itemList = [];
+        if (data !== null) {
+          const storedList = Object.entries(data);
+          storedList.forEach(([key, value]) => {
+            if (key !== 'id') {
+              itemList.push(value);
+            }
+          });
+        }
+        return isLoading ? <div>Loading</div> : <List items={itemList} />;
       }}
     />
   );
