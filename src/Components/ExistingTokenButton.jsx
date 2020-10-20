37b406000bd5797f2619ee732eb8a22e0e1e4bb1
Gabrielle Jacobs <GabbyJ424@gmail.com>
2020-10-20 11:37:08 -0500

    Added functionality to ExistingTokenButton so user can share a token and is alerted if token doesn't exist. Fixed NewTokenButton state.
    Co-authored-by: Gabrielle Jacobs <gabbyj424@gmail.com>
    Co-authored-by: Eden Bekele <edbek@yahoo.com>

--- src/Components/ExistingTokenButton.jsx
+++ src/Components/ExistingTokenButton.jsx
@@ -1,20 +1,70 @@
-import React from 'react';
-import { getListToken } from '../lib/tokens';
-// import { useHistory } from 'react-router-dom';
+/*
+NOTES:
+
+What Works:
+- Create new token (broke when we moved to Home but is fixed)
+- If token doesn't exist, proper pop up shows up
+- If token exists, it saves to localStorage
+- With token, /list shows correct list
+-history.push redirects
+
+What Doesn't Work:
+- Empty string breaks app. Catch error code not working or an else if or .empty
+
+Fixed/ Answered:
+- history.push not working for redirect and I have no clue why (having to refresh to see list contents)
+  - Update: Needed a useState to refresh browser
+- Idk if important, but specifically 'test-token' breaks code, even on production (use 'rev vast petty' for example token)
+  - Update: test-token isn't an array in firestore. Others work
+
+
+*/
+
+import React, { useState } from 'react';
+//import getListToken, { generateListToken } from "../lib/tokens";
 import { db } from '../lib/firebase';
+import { useHistory } from 'react-router-dom';
 
-export default function ExistingTokenButton({ token }) {
-  // let history = useHistory();
+export default function ExistingTokenButton({ setToken }) {
+  const [input, setInput] = useState('');
 
-  const handleClick = () => {
-    console.log(db.collection('lists').doc('test-token'));
+  let history = useHistory();
+
+  const handleClick = (event) => {
+    event.preventDefault(); //works. needed for onChange and onClick to work
+    //const trimmed_input = input.trim();
+
+    db.collection('lists')
+      .doc(input)
+      .onSnapshot(function (querySnapshot) {
+        if (querySnapshot.exists) {
+          //alert(input); //works
+
+          localStorage.setItem('token', input); //works
+          // localStorage.getItem('token');
+          setToken(input);
+          history.push('/list'); //DOESN'T WORK *******
+        } else {
+          alert('Shopping list does not exist. Try again or get a new token'); //works
+        }
+      });
   };
 
   return (
     <div>
-      <button onClick={handleClick}>Join Existing List</button>
-      <br />
-      <input></input>
+      <form>
+        <label>Join an existing list:</label>
+        <input
+          label="Enter Token"
+          id="join-list"
+          defaultValue=""
+          onChange={(event) => setInput(event.target.value)} //works. needed to work onClick
+        ></input>
+        <button type="submit" onClick={handleClick}>
+          Join List
+        </button>{' '}
+        {/*works*/}
+      </form>
     </div>
   );
 }
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import NewTokenButton from './NewTokenButton';
 import ExistingTokenButton from './ExistingTokenButton';
 
-function Home() {
+export default function Home({ setToken }) {
   return (
     <div>
       <h1>Welcome to Your Shopping List</h1>
-      <NewTokenButton />
-      <ExistingTokenButton />
+      <NewTokenButton setToken={setToken} />{' '}
+      {/*Had to pass setToken down from Routes > Home to Home > Button so NewToken button still works */}
+      <br />
+      <ExistingTokenButton setToken={setToken} />
     </div>
   );
 }
-
-export default Home;
