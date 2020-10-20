e8981524bdf040feb5a4678a9c1eb9ebe75ecab0
Gabrielle Jacobs <GabbyJ424@gmail.com>
2020-10-20 17:01:16 -0500

    Cleaned up code and comments.

--- src/Components/ExistingTokenButton.jsx
+++ src/Components/ExistingTokenButton.jsx
@@ -1,40 +1,3 @@
-/*
-NOTES:
-
-What Works:
-- Create new token (broke when we moved to Home but is fixed)
-- If token doesn't exist, proper pop up shows up
-- If token exists, it saves to localStorage
-- With token, /list shows correct list
-
-Fixed/ Answered:
-- history.push not working for redirect and I have no clue why (having to refresh to see list contents)
-  - Update: Needed a useState to refresh browser
-- Idk if important, but specifically 'test-token' breaks code, even on production (use 'rev vast petty' for example token)
-  - Update: test-token isn't an array in firestore. Others work
-
-What Doesn't Work:
-- Empty string breaks app. Catch error code not working or an else if or .empty
-  - TRIED
-    input === ""
-    input === null
-    input === undefined
-    input === "" || input === null || input === undefined
-    querySnapshot === ""
-    querySnapshot === null
-    
-    input.empty
-    querySnapshot.empty
-    
-    .catch((error) => {
-            console.log('Error updating document:', error);
-          });
-
-    {errors.EnterToken && "Enter valid token"}
-
-
-*/
-
 import React, { useState } from 'react';
 import { db } from '../lib/firebase';
 import { useHistory } from 'react-router-dom';
@@ -67,8 +30,6 @@ export default function ExistingTokenButton({ setToken }) {
   return (
     <div className="token">
       <form>
-        {/* <label htmlFor="joinList"><p>Join an existing list:</p></label> */}
-
         <input
           label="Enter Token"
           name="EnterToken"
